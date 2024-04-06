4eb39dbacce8a4991b8323a005f50072c8c7563d
222207 <222207@DESKTOP-ECOFDM2>
2024-04-06 12:58:33 +0300

    Добавьте файлы проекта.

---
+++ chatix/MainMenu.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chatix
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void StartChating_Click(object sender, RoutedEventArgs e)
+        {
+            //NavigationService.Navigate(new ChatiX());
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            //Application.Current.Shutdown();
+        }
+
+        private void DoubleAnimation_Completed_StCH(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new ChatiX());
+        }
+
+        private void DoubleAnimation_Completed_Ex(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
